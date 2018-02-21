8e4567583209bc904557afd124a850ba700af56b
Konrad Krakowiak <konrad.krakowiak@polidea.com>
2018-02-21 15:12:14 +0100

    Added log level dialog

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ble_lib_example/ui/ble_start_screen.dart';
 import 'package:flutter_ble_lib_example/ui/ble_devieces_screen.dart';
 import 'package:flutter_ble_lib_example/ui/screen_on_boarding.dart';
 import 'package:flutter_ble_lib_example/ui/screen_names.dart' as ScreenNames;
@@ -15,8 +14,6 @@ void main() {
       brightness: Brightness.dark,
     ),
     routes: <String, WidgetBuilder>{
-      ScreenNames.bleStartScreen: (
-          BuildContext context) => new BleStartScreen(),
       ScreenNames.bleDevicesScreen: (
           BuildContext context) => new BleDevicesScreen(),
     },
--- example/lib/ui/screen_names.dart
+++ example/lib/ui/screen_names.dart
@@ -1,2 +1,1 @@
-const bleStartScreen = "/bleStartScreen";
 const bleDevicesScreen = "/bleDevicesScreen";
