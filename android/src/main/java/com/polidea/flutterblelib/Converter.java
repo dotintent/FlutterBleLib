63e383cd3ecb7e706a7b45c17f827e4c6a1fcc57
Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>
2018-05-18 17:49:05 +0200

    Set isConnected field in BleDeviceMessage according to RxBleDevice connection state.
    Signed-off-by: Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>

--- android/src/main/java/com/polidea/flutterblelib/Converter.java
+++ android/src/main/java/com/polidea/flutterblelib/Converter.java
@@ -142,11 +142,15 @@ class Converter {
     }
 
     BleData.BleDeviceMessage convertToBleDeviceMessage(RxBleDevice device, int mtu, int rssi) {
+        final boolean isConnected = device.getConnectionState()
+                .equals(RxBleConnection.RxBleConnectionState.CONNECTED);
+
         return BleData.BleDeviceMessage.newBuilder()
                 .setId(stringUtils.safeNullInstance(device.getMacAddress()))
                 .setName(stringUtils.safeNullInstance(device.getName()))
                 .setMtu(mtu)
                 .setRssi(rssi)
+                .setIsConnected(isConnected)
                 .build();
     }
 
