66406080c987dda61556abe3b6732fe3eb446b92
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-09-13 17:58:34 +0200

    [lib][scan-result] fix: json parsing

--- lib/scan_result.dart
+++ lib/scan_result.dart
@@ -1,6 +1,6 @@
 part of flutter_ble_lib;
 
-abstract class Metadata {
+abstract class _Metadata {
   static const String ID = "id";
   static const String NAME = "name";
   static const String RSSI = "rssi";
@@ -25,47 +25,46 @@ class ScanResult {
   AdvertisementData advertisementData;
 
   ScanResult.fromJson(Map<String, dynamic> json)
-      : deviceId = json[Metadata.ID],
-        deviceName = json[Metadata.NAME],
-        rssi = json[Metadata.RSSI],
-        mtu = json[Metadata.MTU],
-        isConnectable = json[Metadata.IS_CONNECTABLE],
-        overflowServiceUUIDs = json[Metadata.OVERFLOW_SERVICE_UUIDS],
-        advertisementData = AdvertisementData.fromJson(json);
+      : deviceId = json[_Metadata.ID],
+        deviceName = json[_Metadata.NAME],
+        rssi = json[_Metadata.RSSI],
+        mtu = json[_Metadata.MTU],
+        isConnectable = json[_Metadata.IS_CONNECTABLE],
+        overflowServiceUUIDs = json[_Metadata.OVERFLOW_SERVICE_UUIDS],
+        advertisementData = AdvertisementData._fromJson(json);
 }
 
 class AdvertisementData {
-  Uint8List manufacturerData;
-  Map<String, Uint8List> serviceData;
+  Int8List manufacturerData;
+  Map<String, Int8List> serviceData;
   List<String> serviceUUIDs;
   String localName;
   int txPowerLevel;
   List<String> solicitedServiceUUIDs;
 
   AdvertisementData._fromJson(Map<String, dynamic> json)
-      : manufacturerData = _base64ToUint8ListOrNull(Metadata.MANUFACTURER_DATA),
+      : manufacturerData = _decodeBase64OrNull(_Metadata.MANUFACTURER_DATA),
         serviceData = _getServiceDataOrNull(json),
-        serviceUUIDs = _mapToStringsListOrNull(json[Metadata.SERVICE_UUIDS]),
-        localName = json[Metadata.LOCAL_NAME],
-        txPowerLevel = json[Metadata.TX_POWER_LEVEL],
-        solicitedServiceUUIDs = _mapToStringsListOrNull(json[Metadata.SOLICITED_SERVICE_UUIDS]);
+        serviceUUIDs = _mapToListOfStringsOrNull(json[_Metadata.SERVICE_UUIDS]),
+        localName = json[_Metadata.LOCAL_NAME],
+        txPowerLevel = json[_Metadata.TX_POWER_LEVEL],
+        solicitedServiceUUIDs =
+            _mapToListOfStringsOrNull(json[_Metadata.SOLICITED_SERVICE_UUIDS]);
 
-  static Map<String, Uint8List> _getServiceDataOrNull(
+  static Map<String, Int8List> _getServiceDataOrNull(
       Map<String, dynamic> json) {
-    Map<String, dynamic> rawServiceData = json[Metadata.SERVICE_DATA];
-    return rawServiceData?.map((key, value) {
-      return MapEntry(key, base64Decode(value));
-    });
+    return json[_Metadata.SERVICE_DATA]?.map(
+      (key, value) => MapEntry(key, Int8List.fromList(base64Decode(value))),
+    );
   }
 
-  static Uint8List _base64ToUint8ListOrNull(String base64Value) {
+  static Int8List _decodeBase64OrNull(String base64Value) {
     if (base64Value != null)
-      return base64.decode(base64Value);
+      return Int8List.fromList(base64.decode(base64Value));
     else
       return null;
   }
 
-  static List<String> _mapToStringsListOrNull(List<dynamic> values) {
-    return values?.cast();
-  }
+  static List<String> _mapToListOfStringsOrNull(List<dynamic> values) =>
+      values?.cast();
 }
