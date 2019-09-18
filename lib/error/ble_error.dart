7128d4803881ee3c7dff4dac3d1d19a60884f686
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-09-18 17:43:32 +0200

    [lib][ble-error] rename internal metadata class

--- lib/error/ble_error.dart
+++ lib/error/ble_error.dart
@@ -1,4 +1,4 @@
-abstract class _Metadata {
+abstract class _BleErrorMetadata {
   static const String ERROR_CODE = "errorCode";
   static const String ATT_ERROR_CODE = "attErrorCode";
   static const String ANDROID_ERROR_CODE = "androidErrorCode";
@@ -24,13 +24,13 @@ class BleError {
   String internalMessage;
 
   BleError.fromJson(Map<String, dynamic> json)
-      : errorCode = json[_Metadata.ERROR_CODE],
-        attErrorCode = json[_Metadata.ATT_ERROR_CODE],
-        androidErrorCode = json[_Metadata.ANDROID_ERROR_CODE],
-        reason = json[_Metadata.REASON],
-        deviceID = json[_Metadata.DEVICE_ID],
-        serviceUUID = json[_Metadata.SERVICE_UUID],
-        characteristicUUID = json[_Metadata.CHARACTERISTIC_UUID],
-        descriptorUUID = json[_Metadata.DESCRIPTOR_UUID],
-        internalMessage = json[_Metadata.INTERNAL_MESSAGE];
+      : errorCode = json[_BleErrorMetadata.ERROR_CODE],
+        attErrorCode = json[_BleErrorMetadata.ATT_ERROR_CODE],
+        androidErrorCode = json[_BleErrorMetadata.ANDROID_ERROR_CODE],
+        reason = json[_BleErrorMetadata.REASON],
+        deviceID = json[_BleErrorMetadata.DEVICE_ID],
+        serviceUUID = json[_BleErrorMetadata.SERVICE_UUID],
+        characteristicUUID = json[_BleErrorMetadata.CHARACTERISTIC_UUID],
+        descriptorUUID = json[_BleErrorMetadata.DESCRIPTOR_UUID],
+        internalMessage = json[_BleErrorMetadata.INTERNAL_MESSAGE];
 }
