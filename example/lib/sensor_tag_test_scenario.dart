a49be97c4212e4caa8805cc8975fe3f8c7539a55
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-09-25 16:29:41 +0200

    [example] add: read/write test cases for service and characteristic

--- example/lib/sensor_tag_test_scenario.dart
+++ example/lib/sensor_tag_test_scenario.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 
 import 'sensor_tag_config.dart';
+import 'util/pair.dart';
 
 typedef Logger = Function(String);
 
@@ -11,6 +12,7 @@ class TestScenario {
   BleManager bleManager = BleManager.getInstance();
   bool deviceConnectionAttempted = false;
   StreamSubscription monitoringStreamSubscription;
+
   Future<void> runTestScenario(Logger log, Logger logError) async {
     log("CREATING CLIENT...");
     await bleManager.createClient(
@@ -40,6 +42,7 @@ class TestScenario {
       logError(error);
     });
   }
+
   Future<void> _tryToConnect(
       Peripheral peripheral, Logger log, Logger logError) async {
     log("OBSERVING connection state \nfor ${peripheral.name},"
@@ -92,6 +95,7 @@ class TestScenario {
         })
         .then((characteristics) => characteristics.forEach((characteristic) =>
             log("Found characteristic \n ${characteristic.uuid}")))
+        .then((_) => log("Test read/write characteristic on device"))
         .then((_) {
           log("Turn off temperature update");
           return peripheral.writeCharacteristic(
@@ -124,6 +128,84 @@ class TestScenario {
         })
         .then((data) {
           log("Temperature value ${data.value}");
+        })
+        .then((_) => log("Test read/write characteristic on service"))
+        .then((_) async {
+          log("Turn off temperature update");
+          Service service = await peripheral.services().then((services) =>
+              services.firstWhere((service) =>
+                  service.uuid ==
+                  SensorTagTemperatureUuids.temperatureService.toLowerCase()));
+          await service.writeCharacteristic(
+            SensorTagTemperatureUuids.temperatureConfigCharacteristic,
+            Uint8List.fromList([0]),
+            false,
+          );
+          return service;
+        })
+        .then((service) =>
+            Future.delayed(Duration(seconds: 1)).then((_) => service))
+        .then((service) async {
+          CharacteristicWithValue dataCharacteristic =
+              await service.readCharacteristic(
+                  SensorTagTemperatureUuids.temperatureDataCharacteristic);
+          return Pair(service, dataCharacteristic);
+        })
+        .then((serviceAndCharacteristic) {
+          log("Temperature value ${serviceAndCharacteristic.second.value}");
+          return serviceAndCharacteristic.first;
+        })
+        .then((service) async {
+          log("Turn on temperature update");
+          Characteristic configCharacteristic =
+              await service.writeCharacteristic(
+                  SensorTagTemperatureUuids.temperatureConfigCharacteristic,
+                  Uint8List.fromList([1]),
+                  false);
+          return Pair(service, configCharacteristic);
+        })
+        .then((serviceAndConfigCharacteristic) =>
+            Future.delayed(Duration(seconds: 1))
+                .then((_) => serviceAndConfigCharacteristic))
+        .then((serviceAndConfigCharacteristic) async {
+          CharacteristicWithValue dataCharacteristic =
+              await serviceAndConfigCharacteristic.first.readCharacteristic(
+                  SensorTagTemperatureUuids.temperatureDataCharacteristic);
+          return Pair(
+              serviceAndConfigCharacteristic.second, dataCharacteristic);
+        })
+        .then((configAndDataCharacteristics) {
+          log("Temperature value ${configAndDataCharacteristics.second.value}");
+          log("Test read/write characteristic on characteristic");
+          return configAndDataCharacteristics.first;
+        })
+        .then((characteristic) =>
+            Future.delayed(Duration(seconds: 1)).then((_) => characteristic))
+        .then((characteristic) async {
+          log("Turn off temperature update");
+          await characteristic.write(Uint8List.fromList([0]), false);
+          return characteristic;
+        })
+        .then((characteristic) =>
+            Future.delayed(Duration(seconds: 1)).then((_) => characteristic))
+        .then((characteristic) async {
+          Uint8List value = await characteristic.read();
+          return Pair(characteristic, value);
+        })
+        .then((characteristicAndValue) {
+          log("Temperature config value ${characteristicAndValue.second}");
+          return characteristicAndValue.first;
+        })
+        .then((characteristic) async {
+          log("Turn on temperature update");
+          await characteristic.write(Uint8List.fromList([1]), false);
+          return characteristic;
+        })
+        .then((characteristic) =>
+            Future.delayed(Duration(seconds: 1)).then((_) => characteristic))
+        .then((characteristic) => characteristic.read())
+        .then((value) {
+          log("Temperature config value $value");
         })
         .then((_) {
           log("WAITING 10 SECOND BEFORE DISCONNECTING");
