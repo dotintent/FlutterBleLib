71f1b085997377b03800ad58e35906abee9443c9
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-09-26 22:20:34 +0200

    [lib] parse incoming platform errors in all bridge calls

--- lib/internal/bridge/bluetooth_state_mixin.dart
+++ lib/internal/bridge/bluetooth_state_mixin.dart
@@ -1,17 +1,23 @@
 part of internal_bridge_lib;
 
-
 mixin BluetoothStateMixin on FlutterBLE {
   Future<void> enableRadio(String transactionId) async {
-    await _methodChannel.invokeMethod(MethodName.enableRadio, <String, dynamic>{
-      ArgumentName.transactionId: transactionId,
-    });
+    await _methodChannel.invokeMethod(
+      MethodName.enableRadio,
+      <String, dynamic>{
+        ArgumentName.transactionId: transactionId,
+      },
+    ).catchError((errorJson) =>
+        Future.error(BleError.fromJson(jsonDecode(errorJson.details))));
   }
 
   Future<void> disableRadio(String transactionId) async {
-    await _methodChannel
-        .invokeMethod(MethodName.disableRadio, <String, dynamic>{
-      ArgumentName.transactionId: transactionId,
-    });
+    await _methodChannel.invokeMethod(
+      MethodName.disableRadio,
+      <String, dynamic>{
+        ArgumentName.transactionId: transactionId,
+      },
+    ).catchError((errorJson) =>
+        Future.error(BleError.fromJson(jsonDecode(errorJson.details))));
   }
 }
