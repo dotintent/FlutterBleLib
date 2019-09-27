9c739499ef2788f0c2fadce7c1c8e7997ef24388
Mikołaj Kojdecki <mikolaj.kojdecki@polidea.com>
2019-09-27 22:18:51 +0200

    Refactor test scenario (#137)
    * Refactor example's test scenario
    
    * After review fixes
    
    * Remove obsolete scenario

--- example/lib/test_scenarios/test_scenarios.dart
+++ example/lib/test_scenarios/test_scenarios.dart
@@ -1,9 +1,12 @@
 library test_scenarios;
 
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 
+import '../sensor_tag_config.dart';
+
 part 'base.dart';
-part 'sensor_tag_test_scenario.dart';
 part 'bluetooth_state_toggle_scenario.dart';
+part 'sensor_tag_test_scenario.dart';
