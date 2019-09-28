b6220c53727b4ba4514abe14df98a51f13705a0d
pawelByszewski <pawell.b@gmail.com>
2019-09-28 02:07:00 +0200

    modify sample

--- example/lib/device_details/device_details_bloc.dart
+++ example/lib/device_details/device_details_bloc.dart
@@ -111,7 +111,7 @@ class DeviceDetailsBloc {
       _connectionStateController.add(connectionState);
     });
 
-    SensorTagTestScenario(peripheral, log, logError).runTestScenario();
+    SensorTagTestScenario(_bleManager, peripheral, log, logError).runTestScenario();
   }
 }
 
