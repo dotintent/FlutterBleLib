edd0fc5566f2d95357bd6fcae9d9f10dec7e5f07
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2019-09-30 15:44:01 +0200

    Fix found issues

--- example/lib/test_scenarios/sensor_tag_test_scenario.dart
+++ example/lib/test_scenarios/sensor_tag_test_scenario.dart
@@ -32,11 +32,21 @@ class SensorTagTestScenario {
   }
 
   Future<void> _cancelTransaction() async {
+    log("Starting operation to cancel...");
     peripheral
         .discoverAllServicesAndCharacteristics(transactionId: "test")
-        .catchError((error) => logError(error));
+        .catchError((error) {
+      BleError bleError = error as BleError;
+      return logError("Cancelled operation caught an error: "
+          "error code ${bleError.errorCode},"
+          " reason: ${bleError.reason}");
+    });
+    log("Operation to cancel started: discover all"
+        " services and characteristics");
 
-    bleManager.cancelTransaction("test");
+    log("Cancelling operation...");
+    await bleManager.cancelTransaction("test");
+    log("Operation cancelled!");
   }
 
   Future<void> _discovery() async {
@@ -202,7 +212,7 @@ class SensorTagTestScenario {
 
     log("Reading characteristic value");
     Uint8List value = await characteristic.read();
-    log("Read temperature config value ${_convertToTemperature(value)}C");
+    log("Read temperature config value $value");
 
     log("Turning on temperature update");
     await characteristic.write(Uint8List.fromList([1]), false);
@@ -210,7 +220,7 @@ class SensorTagTestScenario {
 
     log("Reading characteristic value");
     value = await characteristic.read();
-    log("Read temperature config value ${_convertToTemperature(value)}C");
+    log("Read temperature config value $value");
 
     return peripheral;
   }
@@ -225,14 +235,15 @@ class SensorTagTestScenario {
 
   Future<void> _fetchConnectedDevice() async {
     log("Fetch connected devices");
-    List<Peripheral> peripherals =
-        await bleManager.connectedDevices([peripheral.identifier]);
+    List<Peripheral> peripherals = await bleManager
+        .connectedDevices([SensorTagTemperatureUuids.temperatureService]);
     peripherals.forEach((peripheral) => log("\t${peripheral.toString()}"));
   }
 
   Future<void> _fetchKnownDevice() async {
     log("Fetch known devices");
-    List<Peripheral> peripherals = await bleManager.knownDevices([]);
+    List<Peripheral> peripherals =
+        await bleManager.knownDevices([peripheral.identifier]);
     peripherals.forEach((peripheral) => log("\t${peripheral.toString()}"));
   }
 
