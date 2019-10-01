07c6991ff376665a777b3d469488b0b395031c15
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2019-10-01 10:24:31 +0200

    Remove unused method names

--- android/src/main/java/com/polidea/blemulator/bridging/constants/PlatformMethodName.java
+++ android/src/main/java/com/polidea/blemulator/bridging/constants/PlatformMethodName.java
@@ -3,9 +3,6 @@ package com.polidea.blemulator.bridging.constants;
 public interface PlatformMethodName {
     String SIMULATE = "simulate";
 
-    String SET_ADAPTER_STATE = "setAdapterState";
     String PUBLISH_SCAN_RESULT = "publishScanResult";
-    String SET_CONNECTION_STATE = "";
-    String NEW_CHARACTERISTIC_VALUE_OBSERVED = "newCharacteristicValueObserved";
 }
 
