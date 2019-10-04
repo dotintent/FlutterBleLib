dbc2b616492da9d64a2cba628c43f66efc5f3a99
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-10-04 18:15:44 +0200

    [simulated-characteristic] add properties flags & make read() asynchronous

--- lib/blemulator/simulation/simulated_characteristic.dart
+++ lib/blemulator/simulation/simulated_characteristic.dart
@@ -3,19 +3,32 @@ part of blemulator;
 class SimulatedCharacteristic {
   final String uuid;
   final int id;
-  SimulatedService _service; //TODO is it needed?
+  SimulatedService service;
   Uint8List value;
   final String convenienceName;
+  bool isReadable;
+  bool isWritableWithResponse;
+  bool isWritableWithoutResponse;
+  bool isNotifiable;
+  bool isIndicatable;
 
   StreamController<Uint8List> _streamController;
 
-  SimulatedCharacteristic(
-      {@required this.uuid, @required this.value, this.convenienceName})
-      : id = IdGenerator().nextId();
+  SimulatedCharacteristic({
+    @required this.uuid,
+    @required this.value,
+    this.convenienceName,
+    this.isReadable = true,
+    this.isWritableWithResponse = true,
+    this.isWritableWithoutResponse = true,
+    this.isNotifiable = false,
+    this.isIndicatable = false,
+  }) : id = IdGenerator().nextId();
 
-  void attachToService(SimulatedService service) => _service = service;
+  void attachToService(SimulatedService service) => this.service = service;
 
-  Uint8List read() => value;
+  Future<Uint8List> read() =>
+      isReadable ? value : Future.error("This characteristic is not readable");
 
   void write(Uint8List value) {
     this.value = value;
