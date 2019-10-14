d4287f41b9e4577108f748828308a59d00ff22f6
Bartosz Wilk <bartosz.wilk@polidea.com>
2019-10-14 16:23:12 +0200

    [blemulator][dart-value-decoder][service] remove internal metadata contract

--- android/src/main/java/com/polidea/blemulator/bridging/decoder/ServiceDartValueDecoder.java
+++ android/src/main/java/com/polidea/blemulator/bridging/decoder/ServiceDartValueDecoder.java
@@ -2,6 +2,7 @@ package com.polidea.blemulator.bridging.decoder;
 
 import android.bluetooth.BluetoothGattService;
 
+import com.polidea.blemulator.bridging.constants.SimulationArgumentName;
 import com.polidea.multiplatformbleadapter.Service;
 import com.polidea.multiplatformbleadapter.utils.UUIDConverter;
 
@@ -10,14 +11,9 @@ import java.util.UUID;
 
 public class ServiceDartValueDecoder {
 
-    private interface Metadata {
-        String SERVICE_IDENTIFIER = "serviceId";
-        String SERVICE_UUID = "serviceUuid";
-    }
-
     public Service decode(String deviceId, Map<String, Object> values) {
-        int serviceId = (int) values.get(Metadata.SERVICE_IDENTIFIER);
-        UUID serviceUuid = UUIDConverter.convert((String) values.get(Metadata.SERVICE_UUID));
+        int serviceId = (int) values.get(SimulationArgumentName.SERVICE_ID);
+        UUID serviceUuid = UUIDConverter.convert((String) values.get(SimulationArgumentName.SERVICE_UUID));
         BluetoothGattService bluetoothGattService = new BluetoothGattService(serviceUuid, BluetoothGattService.SERVICE_TYPE_PRIMARY);
         return new Service(serviceId, deviceId, bluetoothGattService);
     }
