8b997b0f7648fc56cbe739183d47d84c79b2edd6
pawelByszewski <pawell.b@gmail.com>
2019-10-21 15:46:59 +0200

    fix build

--- lib/internal/bridge/internal_bridge_lib.dart
+++ lib/internal/bridge/internal_bridge_lib.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_ble_lib/flutter_ble_lib.dart';
