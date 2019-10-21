7e22585f2cd0a2ae633d10afccd95508f106f5d6
Tomasz Bogusz <tomo.bogusz@gmail.com>
2019-10-21 12:21:49 +0200

    [ios] Add readEvent delegate to DartValueHandler

--- ios/Classes/Blemulator/Bridging/DartValueHandler.h
+++ ios/Classes/Blemulator/Bridging/DartValueHandler.h
@@ -1,6 +1,7 @@
 #import "FlutterMethodCallHandler.h"
 #import "ScannedPeripheral.h"
 #import "ConnectionStateEvent.h"
+#import "Characteristic.h"
 
 @protocol DartValueHandlerScanEventDelegate
 
@@ -14,11 +15,18 @@
 
 @end
 
+@protocol DartValueHandlerReadEventDelegate
+
+- (void)dispatchDartValueHandlerReadEvent:(Characteristic *)characteristic;
+
+@end
+
 @interface DartValueHandler : NSObject<FlutterMethodCallHandler>
 
 @property NSMutableArray<NSString *> * observedDeviceIdentifiers;
 
 @property id <DartValueHandlerScanEventDelegate> scanEventDelegate;
 @property id <DartValueHandlerConnectionEventDelegate> connectionEventDelegate;
+@property id <DartValueHandlerReadEventDelegate> readEventDelegate;
 
 @end
