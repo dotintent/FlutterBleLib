5b49caf5c53109aa20b19cf6210db656081cec3c
Bartosz Wilk <bartosz.wilk@polidea.com>
2020-03-03 15:22:41 +0100

    [tests] add: mock classes for managers

---
+++ test/mock/manager_mock.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_ble_lib/src/_managers_for_classes.dart';
+import 'package:mockito/mockito.dart';
+
+class ManagerForServiceMock extends Mock implements ManagerForService {}
+
+class ManagerForCharacteristicMock extends Mock implements ManagerForCharacteristic {}
+
+class ManagerForDescriptorMock extends Mock implements ManagerForDescriptor {}
