0b032c2fe8f8d1905a1afa9a41a4281579d6dbb4
ipcjs <gipcjs@gmail.com>
2020-12-07 17:31:12 +0800

    Refactor and use EventChannel correctly.

--- lib/src/bridge/scanning_mixin.dart
+++ lib/src/bridge/scanning_mixin.dart
@@ -1,11 +1,26 @@
 part of _internal;
 
 mixin ScanningMixin on FlutterBLE {
-  Stream<ScanResult> _scanEvents;
+  EventChannel _eventChannel;
 
-  void _prepareScanEventsStream() {
-    _scanEvents = const EventChannel(ChannelName.scanningEvents)
-        .receiveBroadcastStream()
+  EventChannel get eventChannel =>
+      _eventChannel ??= const EventChannel(ChannelName.scanningEvents);
+
+  Stream<ScanResult> startDeviceScan(
+    int scanMode,
+    int callbackType,
+    List<String> uuids,
+    bool allowDuplicates,
+  ) {
+    return eventChannel
+        .receiveBroadcastStream(
+          <String, dynamic>{
+            ArgumentName.scanMode: scanMode,
+            ArgumentName.callbackType: callbackType,
+            ArgumentName.uuids: uuids,
+            ArgumentName.allowDuplicates: allowDuplicates,
+          },
+        )
         .handleError(
           (errorJson) => throw BleError.fromJson(jsonDecode(errorJson.details)),
           test: (error) => error is PlatformException,
@@ -16,43 +31,7 @@ mixin ScanningMixin on FlutterBLE {
         );
   }
 
-  Stream<ScanResult> startDeviceScan(
-    int scanMode,
-    int callbackType,
-    List<String> uuids,
-    bool allowDuplicates,
-  ) {
-    if (_scanEvents == null) {
-      _prepareScanEventsStream();
-    }
-
-    StreamController<ScanResult> streamController = StreamController.broadcast(
-      onListen: () => _methodChannel.invokeMethod(
-        MethodName.startDeviceScan,
-        <String, dynamic>{
-          ArgumentName.scanMode: scanMode,
-          ArgumentName.callbackType: callbackType,
-          ArgumentName.uuids: uuids,
-          ArgumentName.allowDuplicates: allowDuplicates,
-        },
-      ),
-      onCancel: () => stopDeviceScan(),
-    );
-
-    streamController
-        // When an error occurs, the native will end the stream, so there is no
-        // need to set cancelOnError to true. If set to true, it may cause the
-        // problem that the scan will be terminated immediately when the scan
-        // starts again after an error occurs.
-        .addStream(_scanEvents, cancelOnError: false)
-        .then((_) => streamController?.close());
-
-    return streamController.stream;
-  }
-
   Future<void> stopDeviceScan() async {
     await _methodChannel.invokeMethod(MethodName.stopDeviceScan);
-    _scanEvents = null;
-    return;
   }
 }
