4f0061f9494d012fd91e4a99ffdf98c73dc7aaa4
ipcjs.wf <gipcjs@gmail.com>
2020-12-08 16:09:07 +0800

    refactor scanning on ios

--- ios/Classes/Event/ScanningStreamHandler.m
+++ ios/Classes/Event/ScanningStreamHandler.m
@@ -2,13 +2,45 @@
 #import "ArgumentHandler.h"
 #import "JSONStringifier.h"
 #import "FlutterErrorFactory.h"
+#import "ArgumentKey.h"
 
 @implementation ScanningStreamHandler {
     FlutterEventSink scanResultsSink;
+    id <BleAdapter> bleAdapter;
+}
+
+- (void) attachAdatper:(id<BleAdapter>)adapter {
+    bleAdapter = adapter;
+}
+
+- (void) detachAdapter{
+    [self cancelPreviousScanning:@"detach adapter"];
+    bleAdapter = nil;
+}
+
+- (void) cancelPreviousScanning:(NSString *)message{
+    if (scanResultsSink != nil){
+        NSString *errorCode = @"2"; // OperationCancelled
+        NSDictionary *json = @{@"errorCode": errorCode, @"reason": message};
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:json options:0 error:nil];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        scanResultsSink([FlutterError errorWithCode:@"2" message:message details: jsonString]);
+        scanResultsSink(FlutterEndOfEventStream);
+        scanResultsSink = nil;
+    }
+    [bleAdapter stopDeviceScan];
+}
+
+- (void)stopDeviceScan:(FlutterResult)result {
+    @synchronized (self) {
+        [self cancelPreviousScanning:@"stop device scan"];
+        result(nil);
+    }
 }
 
 - (FlutterError * _Nullable)onCancelWithArguments:(id _Nullable)arguments {
     @synchronized (self) {
+        [bleAdapter stopDeviceScan];
         scanResultsSink = nil;
         return nil;
     }
@@ -16,7 +48,11 @@
 
 - (FlutterError * _Nullable)onListenWithArguments:(id _Nullable)arguments eventSink:(nonnull FlutterEventSink)events {
     @synchronized (self) {
+        [self cancelPreviousScanning:@"Restart the scan"];
         scanResultsSink = events;
+        
+        NSArray* expectedArguments = [NSArray arrayWithObjects:ARGUMENT_KEY_ALLOW_DUPLICATES, nil];
+        [bleAdapter startDeviceScan:[ArgumentHandler stringArrayOrNil:arguments[ARGUMENT_KEY_UUIDS]] options:[ArgumentHandler dictionaryOrNil:expectedArguments in:arguments]];
         return nil;
     }
 }
@@ -27,23 +63,15 @@
             if (!(scanResult.count == 2 &&
                 (scanResult[0] == [NSNull null] || (scanResult[1] == [NSNull null] && [scanResult[0] isKindOfClass:NSString.class])))) {
                 scanResultsSink([FlutterError errorWithCode:@"-1" message:@"Invalid scanResult format." details:nil]);
-                [self onComplete];
+                scanResultsSink(FlutterEndOfEventStream);
             } else {
                 if (scanResult[0] == [NSNull null]) {
                     scanResultsSink([JSONStringifier jsonStringFromJSONObject:scanResult[1]]);
                 } else {
                     scanResultsSink([FlutterErrorFactory flutterErrorFromJSONString:scanResult[0]]);
-                    [self onComplete];
+                    scanResultsSink(FlutterEndOfEventStream);
                 }
             }
-        }
-    }
-}
-
-- (void)onComplete {
-    @synchronized (self) {
-        if (scanResultsSink != nil) {
-            scanResultsSink(FlutterEndOfEventStream);
         }
     }
 }
