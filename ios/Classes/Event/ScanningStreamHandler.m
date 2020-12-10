fd2ee4c3cfb32a3b440a165acb0455c9cff80701
ipcjs.wf <gipcjs@gmail.com>
2020-12-10 19:46:19 +0800

    Don't `endOfStream` on ios

--- ios/Classes/Event/ScanningStreamHandler.m
+++ ios/Classes/Event/ScanningStreamHandler.m
@@ -24,8 +24,7 @@
         NSDictionary *json = @{@"errorCode": errorCode, @"reason": message};
         NSData *jsonData = [NSJSONSerialization dataWithJSONObject:json options:0 error:nil];
         NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        scanResultsSink([FlutterError errorWithCode:@"2" message:message details: jsonString]);
-        scanResultsSink(FlutterEndOfEventStream);
+        scanResultsSink([FlutterError errorWithCode:errorCode message:message details: jsonString]);
         scanResultsSink = nil;
     }
     [bleAdapter stopDeviceScan];
@@ -63,13 +62,11 @@
             if (!(scanResult.count == 2 &&
                 (scanResult[0] == [NSNull null] || (scanResult[1] == [NSNull null] && [scanResult[0] isKindOfClass:NSString.class])))) {
                 scanResultsSink([FlutterError errorWithCode:@"-1" message:@"Invalid scanResult format." details:nil]);
-                scanResultsSink(FlutterEndOfEventStream);
             } else {
                 if (scanResult[0] == [NSNull null]) {
                     scanResultsSink([JSONStringifier jsonStringFromJSONObject:scanResult[1]]);
                 } else {
                     scanResultsSink([FlutterErrorFactory flutterErrorFromJSONString:scanResult[0]]);
-                    scanResultsSink(FlutterEndOfEventStream);
                 }
             }
         }
